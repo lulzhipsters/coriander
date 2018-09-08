03a405ddf329d1e1dd09a048d9bd9bd4bb69eeb6
Lewis Hopkins <lewis.j.hopkins@gmail.com>
2018-09-08 19:10:22 +1000

    Make gitwatcher check for dir / repo exists and clone if it doesn't

--- app/gitWatcher/GitWatcher.ts
+++ app/gitWatcher/GitWatcher.ts
@@ -1,4 +1,6 @@
-import { Repository, Reset, Cred, Commit, Signature, Merge } from "nodegit";
+import { Repository, Reset, Cred, Commit, Signature, Merge, Clone } from "nodegit";
+import * as fs from "fs";
+import config from "../Config";
 
 interface WatchOptions {
     user: string;
@@ -16,7 +18,11 @@ export default class GitWatcher {
         this._options = options;
     }
 
+    
+
     async watch() {
+        await this.initRepo();
+
         while(true){
             let lastCommit = this._currentCommit;
 
@@ -30,13 +36,49 @@ export default class GitWatcher {
         }
     }
 
+    private get credentialCallback() {
+        return () => Cred.userpassPlaintextNew(this._options.user, this._options.password);
+    }
+
+    private async initRepo() {
+        const repoPath = this._options.repoPath;
+
+        //check if the repo already exists
+        const dirExists = await this.dirExists(repoPath);
+
+        //make the dir
+        if(!dirExists){
+            console.log("Making dir: " + repoPath);
+            await this.makeDir(repoPath);
+        }
+
+        //check if the repo exists
+        const repoExists = await this.repoExists(repoPath);
+
+        //init the repo
+        if(!repoExists){
+            console.log(`cloning repo at: ${config.gitRepoUrl} to: ${repoPath}`)
+
+            const cloneOptions = {
+                fetchOpts: {
+                    callbacks: {
+                        certificateCheck: () => 1,
+                        credentials: this.credentialCallback
+                    }
+                }
+            };
+
+            await Clone.clone(config.gitRepoUrl, repoPath, cloneOptions);
+        }
+    }
+
     private async getLatest(){
         console.debug("Checking repo for updates");
 
         const repo = await Repository.open(this._options.repoPath);
         await repo.fetchAll({ 
             callbacks: {
-                credentials: () => Cred.userpassPlaintextNew(this._options.user, this._options.password)
+                credentials: this.credentialCallback
             }
         });
 
@@ -49,6 +91,54 @@ export default class GitWatcher {
     private async delay(time: number){
         return new Promise((resolve, reject) => {
             setTimeout(() => resolve(), time);
+        });
+    }
+
+    private async repoExists(path: string){
+        try {
+            const repo = await Repository.open(path);
+
+            return !!repo;
+
+        } catch (e) {
+        }
+
+        return false;
+    }
+
+    private async dirExists(path: string){
+        try {
+            const stats = await this.getDirStats(path);
+    
+            if(stats.isDirectory) {
+                return true;
+            }
+        } catch (e) {
+        }
+
+        return false;
+    }
+
+    private async makeDir(path: string){
+        return new Promise((resolve, reject) => {
+            fs.mkdir(path, err => {
+                if(err){
+                    reject(err);
+                }
+                resolve();
+            })
+        })
+    }
+
+    private async getDirStats(path: string) {
+        return new Promise<fs.Stats>((resolve, reject) => {
+            fs.stat(path, (err, stats) => {
+                if(err){
+                    reject(err);
+                }
+
+                resolve(stats);
+            })
         });
     }
 }
